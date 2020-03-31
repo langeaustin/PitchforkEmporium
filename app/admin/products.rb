b7efed559907212ac798ea81fba67ab3ce1d8b8c
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-03-31 18:48:27 -0500

    Changed products price to a decimal

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Product do
+  permit_params :name, :description, :price
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-   permit_params :name, :description, :price
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- db/migrate/20200329020502_create_products.rb
+++ db/migrate/20200329020502_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string :name
       t.string :description
-      t.integer :price
+      t.decimal :price
 
       t.timestamps
     end
