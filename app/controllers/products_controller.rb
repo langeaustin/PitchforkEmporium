620c74a28456b7e51b030b299f9d2cb4dcd1d0f0
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-04-06 18:59:49 -0500

    added pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    # @products = Product.all
+    @products = Product.order("name").page(params[:page]).per(5)
   end
 
   def show
