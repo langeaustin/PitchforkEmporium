5df9a651d1f5911fa6d135e1b329fe8edf433287
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-04-06 19:31:37 -0500

    fixed css properties and added a catagories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_224920) do
+ActiveRecord::Schema.define(version: 2020_04_07_003054) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 2020_04_06_224920) do
   create_table "administrators", force: :cascade do |t|
     t.string "admin_name"
     t.string "admin_pass"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "catagories", force: :cascade do |t|
+    t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
