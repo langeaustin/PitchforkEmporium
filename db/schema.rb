946296dfe2303aee054e27887174cc5028c37df0
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-04-06 19:40:03 -0500

    Added category table and category column to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_003054) do
+ActiveRecord::Schema.define(version: 2020_04_07_003801) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_003054) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "provinces", force: :cascade do |t|
