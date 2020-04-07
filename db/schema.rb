1d16bcbc68d39172744357c552da95e4fd0178a2
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-04-06 19:44:47 -0500

    Fixed spelling error on categories table name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_003801) do
+ActiveRecord::Schema.define(version: 2020_04_07_004137) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_003801) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "catagories", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
