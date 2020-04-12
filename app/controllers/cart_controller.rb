32c37368a767dbb90a7d9b3f875d8ea404bb3f0b
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-04-12 17:34:59 -0500

    Added a Order review page

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,5 @@
+class CartController < ApplicationController
+  def show
+    @cats
+  end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,12 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to cart_path
+  end
+
   def load_cart
     @cart = Product.find(session[:cart])
   end
