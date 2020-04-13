3e2b309e175665a357d7f392ad842dbe8f61f093
langeaustin123@gmail.com <alangevin@rrc.ca>
2020-04-13 15:14:58 -0500

    devise login, register and logout are all set. Session cart bugs are fixed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :load_categories
+  before_action :initialize_session
   private
   def load_categories
     @cats = Category.all
     @cart = Product.find(session[:cart])
+  end
+
+  def initialize_session
+    session[:cart] ||= []
   end
 end
